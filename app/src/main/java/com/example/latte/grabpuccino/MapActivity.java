49f3b81be310a46efe10cfe9bf01763099a12a9a
tachakarn <lolopomama@hotmail.com>
2017-06-13 11:18:41 +0700

    Fixed the Google Map API.

--- app/src/main/java/com/example/latte/grabpuccino/MapActivity.java
+++ app/src/main/java/com/example/latte/grabpuccino/MapActivity.java
@@ -1,21 +1,68 @@
 package com.example.latte.grabpuccino;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import android.Manifest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements
+        ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback{
+
+
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    TextView mLatitudeText;
+    TextView mLongitudeText;
+    GoogleMap map;
+    public static final int MY_PERMISSIONS_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        if (mGoogleApiClient == null){
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST);
+        }else{
+            lunchLocation();
+        }
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
@@ -32,14 +79,68 @@ public class MapActivity extends AppCompatActivity {
             textName.setText("Hi, "+email);
         }
 
-
     }
 
+    protected  void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop(){
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private void lunchLocation() {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if(mGoogleApiClient == null){
+                Toast.makeText(this, "null", Toast.LENGTH_SHORT).show();
+
+            }
+
+            if(mLastLocation != null){
+                LatLng myLatLong = new LatLng(
+                        mLastLocation.getLatitude(),
+                        mLastLocation.getLongitude());
+
+                map.moveCamera(CameraUpdateFactory.newLatLng(myLatLong));
+
+                map.addMarker(new MarkerOptions()
+                        .position(myLatLong)
+                        .title("My Location"));
+
+            }
+
+        }
+    }
+
+    public void onConnected(Bundle connectionHint){
+        lunchLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
 
 
     public void signOut(View view) {
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(MapActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
