cb13638f1c981e145948a50ead8d7de03fa04903
Tae Chemist <taechemist@users.noreply.github.com>
2017-06-17 23:16:56 +0700

    Add menu icon, try sign out button

--- app/src/main/java/com/example/latte/grabpuccino/MainActivity.java
+++ app/src/main/java/com/example/latte/grabpuccino/MainActivity.java
@@ -134,4 +134,9 @@ public class MainActivity extends AppCompatActivity {
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+
+    public void signOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+    }
+
 }
